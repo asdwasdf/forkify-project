384a5f30e2cb844354d0bd1b2b00b73d9e1e41d2
nhat <phamminhnhat0208@gmail.com>
2022-10-23 14:47:15 +0700

    v

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = '<YOUR_KEY>';
+export const KEY = 'dfb3a870-1ba8-4c6c-9077-2b676516637b';
 export const MODAL_CLOSE_SEC = 2.5;
